2fa55d44272b259e9c611c4a2be7c90b664775de
ant2l <ant2li1440@gmail.com>
2023-11-15 20:47:46 +0200

    9 lesson

--- coinmetrica/coinprice/urls.py
+++ coinmetrica/coinprice/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('addpage/', addpage, name='addpage'),
     path('login/', login, name='login'),
     path('post/<int:post_id>/', show_price, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category')
 ]
--- coinmetrica/coinprice/views.py
+++ coinmetrica/coinprice/views.py
@@ -9,10 +9,16 @@ menu = [{'title': 'Title', 'url_name': 'home'},
         {'title': 'login', 'url_name': 'login'}]
 def index(request):
     posts = coins.objects.all()
-    context = {'posts': posts,
-               'menu': menu,
-               'title':'Title'
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title':'Title',
+        'cat_selected': 0
     }
+
     return render(request, 'coinprice/index.html', context=context)
 
 def about(request):
@@ -32,3 +38,21 @@ def pageNotFound(request, exception):
 
 def show_price(request, post_id):
     return HttpResponse('coin N = {post_id}')
+
+def show_category(request, cat_id):
+    posts = coins.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Title',
+        'cat_selected': cat_id
+    }
+
+    return render(request, 'coinprice/index.html', context=context)
+
